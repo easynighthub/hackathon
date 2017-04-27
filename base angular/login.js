c842dd2a4b1a73af8835dabfdbfa4eebcffd4b07
Andro <androstoic@gmail.com>
2017-04-27 02:13:17 -0300

    aaaa

--- base angular/login.js
+++ base angular/login.js
@@ -8,14 +8,10 @@ ingresarAdmin.addEventListener('click', function() {
     firebase.auth().signInWithEmailAndPassword(email, password).then(
         function(s){
             console.log(s);
-            firebase.database().ref('/user/' + s.uid).once('value').then(function(snapshot) {
-                if (snapshot.val() != null)
+
                     window.location.href = 'app';
-                else {
-                    alert('ESTE USUARIO NO ES ADMINISTRADOR DE WWW.MARIOGRUA.CL');
-                    firebase.auth().signOut();
-                }
-            });
+
+
         },
         function(e) {
             console.log(e);
